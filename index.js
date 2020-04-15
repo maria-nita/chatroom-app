3fe36f4347c55a5267fe667fe5a8c98ef30a71e7
maria-nita <maria-nita@live.com>
2020-04-15 17:26:23 +0100

    following a tutorial, badly, everythng is broken, will fix tomorrow

--- index.js
+++ index.js
@@ -6,8 +6,27 @@ const uuid = require('uuid/v4');
 
 uuid(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 
+const rooms = { };
+
 app.get('/', function(request, response) {
-	response.render('index.ejs');
+	response.render('index.ejs', { rooms: rooms });
+});
+
+app.post('/room', function(request, response) {
+	if (rooms[request.body.room] != null) {
+		return response.redirect('/');
+	}
+	rooms[request.body.room] = { users: {} };
+	response.redirect(request.body.room);
+	//send message that new room was created
+	io.emit('room-created', request.body.room);
+});
+
+app.get('/:room', function(request, response) {
+	if (rooms[request.params.room] == null) {
+		return response.redirect('/');
+	}
+	response.render('room.ejs', {roomName: request.params.room});
 });
 
 app.get('/meeting-setup', function(request, response) {
@@ -17,21 +36,35 @@ app.get('/meeting-setup', function(request, response) {
 io.sockets.on('connection', function(socket) {
 	// write all the realtime communication functions here
 
-	socket.on('username', function(username) {
-		socket.username = username;
-		io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
+	socket.on('username', function(room, username) {
+		socket.join(room);
+		rooms[room].users[socket.username] = username;
+		// socket.username = username;
+		socket.to(room).broadcast.emit('is_online', '🔵 <i>' + rooms[room].users[socket.username] + ' join the chat..</i>');
+		// io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
 	});
 
 	socket.on('disconnect', function(username) {
-		io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+		getUserRooms(socket).forEach(room => {
+			io.emit('is_online', '🔴 <i>' + rooms[room].users[socket.username] + ' left the chat..</i>');
+			delete rooms[room].users[socket.username];
+		});
 	})
 
-	socket.on('chat_message', function(message) {
-		io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+	socket.on('chat_message', function(room, message) {
+		socket.to(room).broadcast.emit('chat_message', '<strong>' + rooms[room].users[socket.username] + '</strong>: ' + message);
+		// io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
 	});
 });
 
 const server = http.listen(8080, function() {
 	console.log('listening on *:8080');
-	console.log(uuid());
+	// console.log(uuid());
 });
+
+function getUserRooms(socket) {
+	return Object.entries(rooms).reduce((names, [name, room]) => {
+		if (room.users[socket.username] != null) names.push(name)
+		return names
+	}, [])
+  }
---
+++ public/script.js
@@ -0,0 +1,47 @@
+var socket = io.connect('http://localhost:8080');
+// submit text message without reload/refresh the page
+const chatElements = {
+	form: document.querySelector('form'),
+	text: document.querySelector('#txt'),
+	messages: document.querySelector('#messages')
+}
+
+const roomElements = {
+	container: document.querySelector('#room-container')
+}
+
+if (chatElements.form != null) {
+	// ask username
+	var username = prompt('Please tell me your name');
+	socket.emit('username', roomName, username);
+	
+	chatElements.form.addEventListener('submit', function(e) {
+		e.preventDefault();
+		socket.emit('chat_message', roomName, chatElements.text.value);
+		chatElements.text.value;
+		return false;
+	});
+}
+
+socket.on('room-created', function(room) {
+	const roomEl = document.createElement('div');
+	roomEl.innerText = room;
+	const roomLink = document.createElement('a');
+	roomLink.href = `/${room}`;
+	roomLink.innerText = 'join';
+	roomElements.container.append(roomEl);
+	roomElements.container.append(roomLink);
+});
+
+// append the chat text message
+socket.on('chat_message', function(msg) {
+	const newMessage = `<li>${msg}</li>`;
+	chatElements.messages.insertAdjacentHTML('beforeend', newMessage);
+});
+
+// append text if someone is online
+socket.on('is_online', function(username) {
+	const newUser = `<li>${username}</li>`;
+	chatElements.messages.insertAdjacentHTML('beforeend', newUser);
+});
+
